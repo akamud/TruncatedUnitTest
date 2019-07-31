fb48a6e58f0611555694769db527ac0c28148764
Mahmoud Ali <mahmoud.ali@lambda3.com.br>
2019-07-31 10:31:03 -0300

    Initial commit

---
+++ TruncatedUnitTest.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using TruncatedUnitTest;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            List<MyClass> list = null;
+
+            list.Should().BeEquivalentTo(new List<MyClass>
+            {
+                new MyClass
+                {
+                    MyName = "B"
+                }
+            });
+        }
+    }
+}
---
+++ TruncatedUnitTest/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TruncatedUnitTest
+{
+    public class MyClass
+    {
+        public string MyName { get; set; }
+
+        public void DoSomething() => throw new Exception();
+    }
+}
